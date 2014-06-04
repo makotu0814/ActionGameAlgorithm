3834baa309c9da8695d405ac5aa5a9e8cabc7480
MAKOTO SATO <fumiumi7@gmail.com>
2014-06-04 23:48:37 +0900

    ワープの処理を追加。

---
+++ Assets/Scripts/Move/WarpMan.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpMan : Dush {
+
+    private WoodControl woodCtrl;
+
+    private float speed = 0.1f;
+    private float speedX = 0.0f;
+    private float speedY = 0.0f;
+
+    void Start()
+    {
+        GameObject wood = GameObject.Find("WoodControl");
+        woodCtrl = wood.GetComponent<WoodControl>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        speedX = 0.0f;
+        if (horizontal > 0)
+        {
+            speedX = speed;
+        }
+        else if (horizontal < 0)
+        {
+            speedX = -speed;
+        }
+
+
+        speedY = 0.0f;
+        if (vertical > 0)
+        {
+            speedY = speed;
+        }
+        else if (vertical < 0)
+        {
+            speedY = -speed;
+        }
+
+        transform.position += new Vector3(speedX, speedY, 0.0f);
+
+        if (transform.position.x < -woodCtrl.GetScreenWidth())
+        {
+            transform.position += new Vector3(woodCtrl.GetScreenWidth() * 2, 0.0f, 0.0f);
+        }
+
+        if (transform.position.x > woodCtrl.GetScreenWidth())
+        {
+            transform.position -= new Vector3(woodCtrl.GetScreenWidth() * 2, 0.0f, 0.0f);
+        }
+
+        if (transform.position.y < -woodCtrl.GetScreenHeight())
+        {
+            transform.position += new Vector3(0.0f, woodCtrl.GetScreenHeight() * 2, 0.0f);
+        }
+
+        if (transform.position.y > woodCtrl.GetScreenHeight())
+        {
+            transform.position -= new Vector3(0.0f, woodCtrl.GetScreenHeight() * 2, 0.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/WoodControl.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WoodControl : FloorControl
+{
+    public GameObject tree;
+    public int treeNum = 10;
+
+    private float screenWidth = 0.0f;
+    private float screenHeight = 0.0f;
+
+    protected override void CreateFloors()
+    {
+        Floor floorObj = Instantiate(floor) as Floor;
+        screenWidth = floorObj.GetScreenwWidth();
+        screenHeight = floorObj.GetScreenHeight();
+
+        for (int i = 0; i < treeNum; i++)
+        {
+            float x = Random.Range(-screenWidth / 2, screenWidth / 2);
+            float y = Random.Range(-screenHeight / 2, screenHeight / 2);
+
+            GameObject woodObj = Instantiate(tree) as GameObject;
+            woodObj.transform.parent = transform;
+            woodObj.transform.localPosition = new Vector3(x, y, 0.0f);
+        }
+
+        // フロアを生成しない
+        Destroy(floorObj.gameObject);
+    }
+
+
+    protected override void SetCharaOnFloor()
+    {
+        Vector3 position = new Vector3(0.0f, -screenHeight / 2 + dush.GetHeight() / 2);
+
+        CreateMan(position);
+        CreateMan(position + new Vector3(screenWidth, 0.0f, 0.0f));
+        CreateMan(position + new Vector3(0.0f, -screenHeight, 0.0f));
+        CreateMan(position + new Vector3(screenWidth, -screenHeight, 0.0f));
+    }
+
+    private void CreateMan(Vector3 position)
+    {
+        Dush man = Instantiate(dush) as Dush;
+        man.SetPosition(position);
+    }
+
+    public override float GetScreenWidth()
+    {
+        return screenWidth;
+    }
+
+    public override float GetScreenHeight()
+    {
+        return screenHeight;
+    }
+}
